81ceee242aa12fe07024dc33c73d4242ddec9aed
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2018-07-11 11:15:29 -0300

    Assets-loaded translations
    - Service checks for smarti18n key on localstorage, and uses it if found
    - Service falls back to default.i18n.json if desired locale file is not found
    - Service load translations from /assets/[componentName]/[lang | default].i18n.json.
    - Added a peerDependency on rxjs.

--- projects/smarti18n/src/lib/services/smarti18n-assets.service.ts
+++ projects/smarti18n/src/lib/services/smarti18n-assets.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { TranslateRequestModel } from '../models/translate-request.model';
+import {
+	mergeMap,
+	catchError
+} from 'rxjs/operators';
 
 @Injectable()
 
@@ -10,6 +13,16 @@ export class Smarti18nAssets {
 	) {}
 
 	public getString(componentName: string) {
+		const locale = localStorage.getItem('smarti18n-locale');
+
+		if (locale) {
+			return this.http.get<any>(`/assets/i18n/${componentName}/${locale}.i18n.json`)
+						.pipe(catchError(result => {
+							console.log(result);
+							return this.http.get(`/assets/i18n/${componentName}/default.i18n.json`);
+						}));
+		}
+
 		return this.http.get(`/assets/i18n/${componentName}/default.i18n.json`);
 	}
 }
