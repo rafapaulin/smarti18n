054247291c6c9c9a5a4ceb4708eb32424dc0b9b8
Daniel Alves <dfaobolinho@gmail.com>
2018-07-12 20:46:45 +0000

    translate-pipe: created pipe

---
+++ projects/smarti18n/src/lib/pipes/smarti18n.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { Smarti18nPipe } from './smarti18n.pipe';
+
+describe('Smarti18nPipe', () => {
+	it('create an instance', () => {
+		const pipe = new Smarti18nPipe();
+		expect(pipe).toBeTruthy();
+	});
+});
--- projects/smarti18n/src/lib/smarti18n.module.ts
+++ projects/smarti18n/src/lib/smarti18n.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Smarti18nDirective } from './directives/smarti18n.directive';
 import { Smarti18nService } from './services/smarti18n.service';
+import { Smarti18nPipe } from './pipes/smarti18n.pipe';
 
 @NgModule({
 	declarations: [
-		Smarti18nDirective
+		Smarti18nDirective,
+		Smarti18nPipe
 	],
 	exports: [
-		Smarti18nDirective
+		Smarti18nDirective,
+		Smarti18nPipe
 	],
 	providers: [
 		Smarti18nService
