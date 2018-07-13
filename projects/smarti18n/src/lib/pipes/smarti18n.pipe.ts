c92497ee083a9035595bf36f0a264612dd86f339
Daniel Alves <dfaobolinho@gmail.com>
2018-07-13 22:27:06 +0000

    feature/translate-pipe: finished pipe logic

--- projects/smarti18n/src/lib/pipes/smarti18n.pipe.ts
+++ projects/smarti18n/src/lib/pipes/smarti18n.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform, OnInit, OnDestroy } from '@angular/core';
+import { Pipe, PipeTransform, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
@@ -8,29 +8,58 @@ import { Smarti18nService } from '../services/smarti18n.service';
 	name: 'smarti18n',
 	pure: false
 })
-export class Smarti18nPipe implements PipeTransform, OnInit, OnDestroy {
-	private value: any;
+export class Smarti18nPipe implements PipeTransform, OnDestroy {
+	private path: string;
 	private transformed: string;
-	private unsubscribeAll = new Subject();
+	private unsubscribeAll: Subject<any>;
 
+	/**
+	 *Creates an instance of Smarti18nPipe.
+	 * @param {Smarti18nService} smarti18n
+	 * @memberof Smarti18nPipe
+	 */
 	constructor(private smarti18n: Smarti18nService) { }
 
-	public ngOnInit(): void {
-		this.smarti18n
-			.onLocaleChanged
-			.pipe(takeUntil(this.unsubscribeAll))
-			.subscribe(_ => this.transformed = this.smarti18n.getTranslation(this.value));
-	}
+	/**
+	 * destroy method - called by @angular
+	 *
+	 * @memberof Smarti18nPipe
+	 */
 	public ngOnDestroy(): void {
 		this.unsubscribeAll.next();
 		this.unsubscribeAll.complete();
 	}
 
-	transform(value: any, args?: any): any {
-		if (this.value !== value) {
-			this.value = value;
-			this.transformed = this.smarti18n.getTranslation(this.value);
+	/**
+	 * localizes the input
+	 *
+	 * @param {string} value 		dot notation localizable path
+	 * @param {*} [args] 			args for localized interpolation
+	 * @returns {string}
+	 * @memberof Smarti18nPipe
+	 */
+	public transform(value: string, args?: any): string {
+		console.log('transform', { value });
+		if (!this.unsubscribeAll) {
+			this.init();
+		}
+		if (this.path !== value) {
+			this.path = value;
+			this.transformed = this.smarti18n.getTranslation(this.path);
 		}
 		return this.transformed;
+	}
+
+	/**
+	 * init method
+	 *
+	 * @memberof Smarti18nPipe
+	 */
+	private init(): void {
+		this.unsubscribeAll = new Subject();
+		this.smarti18n
+			.onLocaleChanged
+			.pipe(takeUntil(this.unsubscribeAll))
+			.subscribe(locale => this.transformed = this.smarti18n.getTranslation(this.path));
 	}
 }
