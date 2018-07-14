60c0ff0639d1b60dc662a52dc6fa214a3718e3cc
Daniel Alves <dfaobolinho@gmail.com>
2018-07-14 16:40:17 +0000

    feature/issue_4: fixed deep merge of translations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { TestComponent } from './test/test.component';
 	],
 	imports: [
 		BrowserModule,
+		FormsModule,
 		Smarti18nModule,
 		HttpClientModule
 	],
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Smarti18nService } from 'projects/smarti18n/src/public_api';
+
 @Component({
 	selector: 'app-test',
 	templateUrl: './test.component.html',
 	styleUrls: ['./test.component.css']
 })
 export class TestComponent {
+	public selectLocale = 'pt-br';
 	public isTrue = false;
 	public pipePath = 'pipe.works';
 
-	constructor() { }
+	constructor(private smarti18n: Smarti18nService) { }
+
+	public changeLocale(): void {
+		this.smarti18n.setLocale(this.selectLocale);
+	}
 }
