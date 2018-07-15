369544224251bed9370cc417a62e9c02eb871ac1
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2018-07-15 11:43:46 -0300

    feature/variableInterpolation, Variables check
    - Checking if there is variables to interpolate on the translated string.

--- projects/smarti18n/src/lib/services/smarti18n.service.ts
+++ projects/smarti18n/src/lib/services/smarti18n.service.ts
@@ -85,6 +85,9 @@ export class Smarti18nService {
 	public interpolate(string: string, variables: any) {
 		const stringVars = string.match(/:\w+[^\s:\.\,$\(\)\[\]\*]/g);
 
+		if (!stringVars || stringVars.length <= 0)
+			throw new Error('There is no parsable vars on the string. Please use the ":varName" notation.');
+
 		for (const i in stringVars) {
 			const key = stringVars[i].substring(1);
 
