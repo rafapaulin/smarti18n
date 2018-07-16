e2b2326a514b9273e4a6d31a3d62413e5eb46854
Rafael Eduardo Paulin <r.paulin@cinq.com.br>
2018-07-16 15:21:54 -0300

    feature/pluralization
    - Simple plurarizations (split by |)
    - Complex plurarization
      - Split by |
      - Exact numbers must be curly-braced {n}
      - Ranges must be square-braced and coma-separated [n,m]
        - To infinite operator suported on second member of the ranges [n,*]

--- projects/smarti18n/src/lib/utils/string-utils.ts
+++ projects/smarti18n/src/lib/utils/string-utils.ts
@@ -19,4 +19,40 @@ export class StringUtils {
 
 		return string;
 	}
+
+	public static pluralize(string: string, countVal: number) {
+		countVal = Number(countVal);
+
+		const COMPLEX_COUNT_REGEX = /^(?:(?:{(\d+)})|(?:\[(\d+),(\d+|\*)\]))/;
+
+		if (string.indexOf('|') === -1)
+			throw new Error('There is no plural options. Please use pipe "|" to separe the plural options.');
+
+		const options = string.split('|');
+
+		if (options.every(opt => COMPLEX_COUNT_REGEX.test(opt))) {
+			for (const i in options) {
+				const rangeData = options[i].match(COMPLEX_COUNT_REGEX);
+
+				rangeData.shift();
+
+				let range: any[] = rangeData.filter(val => !!val);
+
+				range = range.map(val => val === '*' ? Number.MAX_SAFE_INTEGER : Number(val));
+
+				if (range[1] && range[0] >= range[1]) throw new Error(`Lower limit must be less than higher limit. (${options[i]})`);
+
+				if (
+					(range.length === 1 && countVal === range[0]) ||
+					(range.length === 2 && (countVal >= range[0] && countVal <= range[1]))
+				)
+					string = options[i].replace(COMPLEX_COUNT_REGEX, '');
+			}
+		} else if (options.some(opt => COMPLEX_COUNT_REGEX.test(opt)))
+			throw new Error('Please provide the quantity notation ({total} or [min, max]) in all pluralization options.');
+		else
+			return countVal > 1 ? options[1] : options[0];
+
+		return string;
+	}
 }
