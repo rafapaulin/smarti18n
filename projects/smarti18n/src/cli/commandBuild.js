312a6fe710bbf860098931bca5822f967f83e334
Daniel Alves <dfaobolinho@gmail.com>
2018-07-23 01:43:16 +0000

    feature/cli: basic funcionality

--- projects/smarti18n/src/cli/commandBuild.js
+++ projects/smarti18n/src/cli/commandBuild.js
@@ -13,18 +13,18 @@ module.exports = class BuildCommand {
 
 	run() {
 		this.verboseCommandInfo();
-		this.build(this.getSrcPath(this.options.project));
+		const projectSrcPath = this.getSrcPath(this.options.project);
+		this.build(projectSrcPath);
+		this.saveLocales(projectSrcPath);
 	}
 
 	build(buildPath) {
 		this.verbose(`- ${buildPath}`);
-		fs.readdir(buildPath, (err, list) => {
-			if (err) throw new Error(err);
-			const sep = this.separateFolders(buildPath, list);
-			sep.folders.forEach(folder => this.build(folder));
-			if (sep.i18n)
-				this.addDataFrom(sep.i18n);
-		});
+		const list = fs.readdirSync(buildPath);
+		const sep = this.separateFolders(buildPath, list);
+		sep.folders.forEach(folder => this.build(folder));
+		if (sep.i18n)
+			this.addDataFromDir(sep.i18n);
 	}
 
 	separateFolders(basePath, list) {
@@ -41,8 +41,45 @@ module.exports = class BuildCommand {
 		};
 	}
 
-	addDataFrom(path) {
-		console.log(chalk.blue(path));
+	addDataFromDir(i18nPath) {
+		const i18nRegex = /(.*)\.i18n\.json/i;
+		const list = fs.readdirSync(i18nPath);
+		this.verbose(`- ${i18nPath}`);
+		list.filter(f => f.endsWith('.i18n.json'))
+			.map(f => ({
+				locale: i18nRegex.exec(f)[1],
+				filePath: path.join(i18nPath, f)
+			}))
+			.forEach(({locale, filePath}) => this.addDataFromFile(locale, filePath));
+	}
+	addDataFromFile(locale, filePath) {
+		this.verbose(`    - ${filePath}`);
+		if (!this.locales[locale])
+			this.locales[locale] = {};
+		this.writeDataToGraph(this.locales[locale], require(filePath));
+	}
+
+	writeDataToGraph(writeData, readData) {
+		const orig = writeData;
+		// create and navigate into base path if available
+		if (readData._basePath) {
+			const basePath = readData._basePath.split('.');
+			basePath.forEach(p => {
+				if (!writeData.hasOwnProperty(p) && p !== '_basePath') {
+					writeData[p] = {};
+					writeData = writeData[p];
+				}
+			})
+		}
+		// write the rest
+		Object.keys(readData).forEach(k => k !== '_basePath' ? writeData[k] = readData[k] : null);
+	}
+
+	saveLocales(saveBasePath) {
+		Object.keys(this.locales).forEach(locale => {
+			const finalPath = path.join(saveBasePath, 'assets', 'i18n', `${locale}.i18n.json`);
+			fs.outputJsonSync(finalPath, this.locales[locale]);
+		});
 	}
 
 	getSrcPath(project = undefined) {
