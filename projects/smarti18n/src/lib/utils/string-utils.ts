88ad335241add428db6c74f60cb8aa3351f95810
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2018-10-12 13:22:57 -0300

    review/fixes - General code cleanup - Code pattern fixes

--- projects/smarti18n/src/lib/utils/string-utils.ts
+++ projects/smarti18n/src/lib/utils/string-utils.ts
@@ -5,23 +5,21 @@ export class StringUtils {
 	 * @param variables Object containing the ```key:value``` pairs to be interpolated.
 	 */
 	public static interpolate(string: string, variables: any): string {
-		if (string) {
-			const stringVars = string.match(/:\w+[^\s:\.\,$\(\)\[\]\*]/g);
+		if (!string) return '';
 
-			if (!stringVars || stringVars.length <= 0)
-			throw new Error('There is no parsable vars on the string. Please use the ":varName" notation.');
+		const stringVars = string.match(/:\w+[^\s:\.\,$\(\)\[\]\*]/g);
 
-			for (const i in stringVars) {
-				const key = stringVars[i].substring(1);
+		if (!stringVars || stringVars.length <= 0)
+		throw new Error('There is no parsable vars on the string. Please use the ":varName" notation.');
 
-				if (variables.hasOwnProperty(key))
-				string = string.replace(stringVars[i], variables[key]);
-			}
+		for (const i in stringVars) {
+			const key = stringVars[i].substring(1);
 
-			return string;
+			if (variables.hasOwnProperty(key))
+			string = string.replace(stringVars[i], variables[key]);
 		}
 
-		return '';
+		return string;
 	}
 
 
@@ -32,44 +30,43 @@ export class StringUtils {
 	 * @returns The pluralized string.
 	 */
 	public static pluralize(string: string, countVal: number): string {
-		if (string) {
-			countVal = Number(countVal);
+		if (!string) return '';
 
-			const COMPLEX_COUNT_REGEX = /^(?:(?:{(\d+)})|(?:\[(\d+),(\d+|\*)\]))/;
+		countVal = Number(countVal);
 
-			if (string.indexOf('|') === -1)
-				throw new Error(`There is no plural options. Please use pipe "|" to separe the plural options. (string: ${string})`);
+		const COMPLEX_COUNT_REGEX = /^(?:(?:{(\d+)})|(?:\[(\d+),(\d+|\*)\]))/;
 
-			const options = string.split('|');
+		if (string.indexOf('|') === -1)
+			throw new Error(`There is no plural options. Please use pipe "|" to separe the plural options. (string: ${string})`);
 
-			if (options.length < 3)
-				throw new Error(`You need to provide at least 3 plural options on your string. (string: ${string})`);
+		const options = string.split('|');
 
-			if (options.every(opt => COMPLEX_COUNT_REGEX.test(opt))) {
-				for (const i in options) {
-					const rangeData = options[i].match(COMPLEX_COUNT_REGEX);
+		if (options.length < 3)
+			throw new Error(`You need to provide at least 3 plural options on your string. (string: ${string})`);
 
-					rangeData.shift();
+		if (options.every(opt => COMPLEX_COUNT_REGEX.test(opt))) {
+			for (const i in options) {
+				const rangeData = options[i].match(COMPLEX_COUNT_REGEX);
 
-					const range = rangeData
-									.filter(val => !!val)
-									.map(val => val === '*' ? Number.MAX_SAFE_INTEGER : Number(val));
+				rangeData.shift();
 
-					if (range[1] && range[0] >= range[1]) throw new Error(`Lower limit must be less than higher limit. (string: ${options[i]})`);
+				const range = rangeData
+								.filter(val => !!val)
+								.map(val => val === '*' ? Number.MAX_SAFE_INTEGER : Number(val));
 
-					if (
-						(range.length === 1 && countVal === range[0]) ||
-						(range.length === 2 && (countVal >= range[0] && countVal <= range[1]))
-					)
-						string = options[i].replace(COMPLEX_COUNT_REGEX, '');
-				}
-			} else if (options.some(opt => COMPLEX_COUNT_REGEX.test(opt)))
-				throw new Error('Please provide the quantity notation ({total} or [min, max]) in all pluralization options.');
-			else
-				return countVal === 0 ? options[0] : (countVal === 1 ? options[1] : options[2]);
+				if (range[1] && range[0] >= range[1]) throw new Error(`Lower limit must be less than higher limit. (string: ${options[i]})`);
 
-			return string;
-		}
-		return '';
+				if (
+					(range.length === 1 && countVal === range[0]) ||
+					(range.length === 2 && (countVal >= range[0] && countVal <= range[1]))
+				)
+					string = options[i].replace(COMPLEX_COUNT_REGEX, '');
+			}
+		} else if (options.some(opt => COMPLEX_COUNT_REGEX.test(opt)))
+			throw new Error('Please provide the quantity notation ({total} or [min, max]) in all pluralization options.');
+		else
+			return countVal === 0 ? options[0] : (countVal === 1 ? options[1] : options[2]);
+
+		return string;
 	}
 }
